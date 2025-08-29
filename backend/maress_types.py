53a14bf4ca40b3a9367c4258f5733bb014582734
Benjamin Schmidt <benschmidt@live.de>
2025-08-29 17:43:26 +0200

    feat(types): ✨ add some custom types

---
+++ backend/maress_types.py
@@ -0,0 +1,54 @@
+from enum import Enum
+from typing import Any, Literal
+
+ZoteroItemKey = str
+ZoteroItemVersion = int
+ZoteroItemLibraryKey = Literal["type", "id", "name"]
+ZoteroItemLinkKey = Literal["href", "type", "title", "length", "alternate"]
+ZoteroItemLinks = dict[ZoteroItemLinkKey, str | int]
+ZoteroItemLibrary = dict[ZoteroItemLibraryKey, str | int | ZoteroItemLinks]
+ZoteroItemMetaCreatedByUser = dict[
+    Literal["id", "username", "name"], str | int | ZoteroItemLinks
+]
+ZoteroItemMeta = dict[
+    Literal["createdByUser", "numChildren"], ZoteroItemMetaCreatedByUser | int
+]
+ZoteroItemDataKey = Literal[
+    "key",
+    "version",
+    "parentItem",
+    "itemType",
+    "linkMode",
+    "title",
+    "accessDate",
+    "url",
+    "note",
+    "contentType",
+    "charset",
+    "filename",
+    "md5",
+    "mtime",
+    "tags",
+    "relations",
+    "dateAdded",
+    "dateModified",
+]
+ZoteroItemData = dict[ZoteroItemDataKey, str | int | list[str] | dict[str, Any]]
+
+ZoteroItem = dict[
+    Literal["key", "version", "library", "links", "meta", "data"],
+    ZoteroItemLibrary | ZoteroItemLinks | ZoteroItemMeta | ZoteroItemData,
+]
+ZoteroItemList = list[ZoteroItem]
+
+class CoordinateExtractionMethod(str, Enum):
+    REGEX = "regex"
+    TABLE_PARSING = "table_parsing"
+    NER = "ner"
+    GEOCODED = "geocoded"
+class CoordinateSourceType(str, Enum):
+    TEXT = "text"
+    TABLE = "table"
+    CAPTION = "caption"
+    MANUAL = "manual"
+    METADATA = "metadata"
