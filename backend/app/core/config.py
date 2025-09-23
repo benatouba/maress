25728d9ced930f2cb703caea463d803073e68b67
Benjamin Schmidt <benschmidt@live.de>
2025-09-23 06:45:41 +0200

    feat(security): ✨ secure cryptography of users api keys

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,25 +1,25 @@
+import os
 import secrets
 import warnings
-from typing import Annotated, Any, Literal
+from typing import Annotated, Any, Literal, Self
 
+from cryptography.fernet import Fernet
 from pydantic import (
     AnyUrl,
     BeforeValidator,
     EmailStr,
-    HttpUrl,
     PostgresDsn,
     computed_field,
     model_validator,
 )
 from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from typing_extensions import Self
 
 
 def parse_cors(v: Any) -> list[str] | str:
     if isinstance(v, str) and not v.startswith("["):
         return [i.strip() for i in v.split(",")]
-    elif isinstance(v, list | str):
+    if isinstance(v, list | str):
         return v
     raise ValueError(v)
 
@@ -34,19 +34,20 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8 * 60
     FRONTEND_HOST: str = "http://localhost:3000"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
     BACKEND_CORS_ORIGINS: Annotated[
-        list[AnyUrl] | str, BeforeValidator(parse_cors)
+        list[AnyUrl] | str,
+        BeforeValidator(parse_cors),
     ] = []
 
     @computed_field  # type: ignore[prop-decorator]
     @property
     def all_cors_origins(self) -> list[str]:
         return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS] + [
-            self.FRONTEND_HOST
+            self.FRONTEND_HOST,
         ]
 
     PROJECT_NAME: str
@@ -98,6 +99,7 @@ class Settings(BaseSettings):
     ZOTERO_USER_ID: str
     ZOTERO_LIBRARY_TYPE: Literal["user", "group"] = "user"
 
+    ENCRYPTION_KEY: str
 
     def _check_default_secret(self, var_name: str, value: str | None) -> None:
         if value == "changethis":
@@ -114,12 +116,11 @@ class Settings(BaseSettings):
     def _enforce_non_default_secrets(self) -> Self:
         self._check_default_secret("SECRET_KEY", self.SECRET_KEY)
         self._check_default_secret("POSTGRES_PASSWORD", self.POSTGRES_PASSWORD)
-        self._check_default_secret(
-            "FIRST_SUPERUSER_PASSWORD", self.FIRST_SUPERUSER_PASSWORD
-        )
+        self._check_default_secret("FIRST_SUPERUSER_PASSWORD", self.FIRST_SUPERUSER_PASSWORD)
         self._check_default_secret("ZOTERO_API_KEY", self.ZOTERO_API_KEY)
         self._check_default_secret("ZOTERO_USER_ID", self.ZOTERO_USER_ID)
         self._check_default_secret("ZOTERO_LIBRARY_TYPE", self.ZOTERO_LIBRARY_TYPE)
+        self._check_default_secret("ENCRYPTION_KEY", self.ENCRYPTION_KEY)
 
         return self
 
